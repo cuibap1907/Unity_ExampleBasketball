5f65106c861dfe7c06a3c3f0b9319b1814ba8f14
tien.levan <tien.levan@gameloft.com>
2019-09-18 17:13:39 +0700

    + add all files

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ball;
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+    public bool holdingBall = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            holdingBall = false;
+            ball.GetComponent<Rigidbody>().useGravity = true;
+            ball.GetComponent<Rigidbody>().AddForce(ballThrowingForce * ball.transform.up);
+        }
+    }
+}
